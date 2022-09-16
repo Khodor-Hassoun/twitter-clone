e12989574f4ba82494fc91a55d9f78e62b2c67bd
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-16 22:52:55 +0300

    added searching api

--- api/search.php
+++ api/search.php
@@ -4,8 +4,9 @@ header("Access-Control-Allow-Origin: *");
 include("connection.php");
 
 $user_id = $_POST["userId"];
+$search = $_POST["search"];
 
-$query = $mysqli->prepare("SELECT name, username, profile_picture FROM users where id = $user_id");
+$query = $mysqli->prepare("SELECT u.name, u.username, u.profile_picture FROM users as u WHERE u.username LIKE '%$search%' and u.id not in (select blocked_id FROM blocks WHERE blocker_id = $user_id) and u.id != $user_id");
 $query->execute();
 $array = $query->get_result();
 

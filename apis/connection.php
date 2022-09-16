67de8748e957fcc7ee7b48552c3561ae80afdd95
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-16 15:24:52 +0300

    Post functional from the form

--- apis/connection.php
+++ apis/connection.php
@@ -2,7 +2,7 @@
     $host = "localhost";
     $db_user = "root";
     $db_pass = null;
-    $db_name = "mydb";
+    $db_name = "twitterdb";
     
     $mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
 ?>

204ab93a32e61a34f3d4e6cc2bdc148e2d36fc53
Ali Ismail <109383562+ali-ismail02@users.noreply.github.com>
2022-09-18 20:38:17 +0300

    Update feed.php
    fixed 1 line bug in the api

--- api/feed.php
+++ api/feed.php
@@ -6,7 +6,7 @@ include("connection.php");
 if(isset($_POST["userId"])){
     $user_id = $_POST["userId"];
 
-    $query = $mysqli->prepare("SELECT u.id, u.name, u.username, u.profile_picture,t.id as tweet, t.date, t.text, t.images FROM `users` as u, tweets as t WHERE u.id = t.id and t.user_id in (SELECT following_id1 from followers where follwer_id = $user_id)");
+    $query = $mysqli->prepare("SELECT u.id, u.name, u.username, u.profile_picture,t.id as tweet, t.date, t.text, t.images FROM `users` as u, tweets as t WHERE u.id = t.user_id and t.user_id in (SELECT following_id1 from followers where follwer_id = $user_id)");
     $query->execute();
     $array = $query->get_result();
 
